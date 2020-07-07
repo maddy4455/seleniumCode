b5027e39a914a96ccee4a9e9a7a3209cb180f9cc
github_username <you@domain.com>
2020-07-07 12:37:42 -0600

    PageObjectLoginTest

--- MavenDemo/src/test/java/pageobjectmodel/LoginTest.java
+++ MavenDemo/src/test/java/pageobjectmodel/LoginTest.java
@@ -1,42 +1,59 @@
 package pageobjectmodel;
 
+import java.util.concurrent.TimeUnit;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 import MyMavenDemo.MavenDemo.BaseTest;
 
-public class LoginTest extends BaseTest {
+public class LoginTest extends BaseTest 
+{
+	
+	 @Test
+	  public void RegistrationTest() throws Exception 
+	  {
+		  openBrowser("chrome");
+		  test.log(Status.INFO, "initializing chrome browser");
+		  
+			driver.get("http://automationpractice.com/index.php");
+			test.log(Status.INFO, "Launching the website :- " + "http://automationpractice.com/index.php");
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+			
+			CustomerRegistration c=new CustomerRegistration(driver);
+			c.CustomerRegistrationPage();
+			
+			String actTitle = driver.getTitle();
+			String expTitle = "My account - My Store";
+			
+			Assert.assertEquals(actTitle,expTitle );
+			
+			System.out.println("The actual page title is :- " + actTitle);
+		  test.log(Status.PASS, "verified the page title :-" + actTitle);
+		  
+		  //driver.quit();
+	  }
 
-	@Test
+
+	@Test (enabled=false)
 	public void Logintest() throws Exception {
 
 		openBrowser("chrome");
+		 test.log(Status.INFO, "initializing chrome browser");
+		 
 		driver.get("http://automationpractice.com/index.php");
-
+		test.log(Status.INFO, "Launching the website :- " + "http://automationpractice.com/index.php");
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
 		LoginPage p = new LoginPage(driver);
 		p.customerLogin();
 		
 		String expMsg = "Authentication failed.";
 		Assert.assertEquals(p.verifyError(), expMsg);
-
-		/*
-		 * driver.findElement(By.linkText("Sign in")).click();
-		 * 
-		 * Thread.sleep(2000);
-		 * 
-		 * driver.findElement(By.id("email")).sendKeys("madd567b@gmail.com");
-		 * driver.findElement(By.id("passwd")).sendKeys("passwrd");
-		 * driver.findElement(By.xpath("//button[@id='SubmitLogin']")).click();
-		 * 
-		 * String actualMsg =
-		 * driver.findElement(By.xpath("//li[contains(text(),'Authentication failed.')]"
-		 * )).getText(); String expMsg = "Authentication failed.";
-		 * 
-		 * Assert.assertEquals(actualMsg, expMsg);
-		 * 
-		 * System.out.println("Error Message is : " + actualMsg);
-		 */
-
+		test.log(Status.PASS, "Verified error message :- "+ expMsg);
+		
 		driver.quit();
 	}
 }
