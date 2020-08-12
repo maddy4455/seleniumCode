4cbc1d99f82cac73d39f56e6f074792bc42db0e8
github_username <you@domain.com>
2020-08-12 13:55:11 -0600

    Updated screen shot utility

---
+++ gtplProject/src/main/java/utilities/Utility.java
@@ -0,0 +1,34 @@
+package utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Utility {
+
+	public static String getScreenshot(WebDriver driver) throws InterruptedException, IOException {
+
+		Date dt = new Date();
+		String filePath = dt.toString().replace(':', '_').replace(' ', '_') + ".html";
+
+		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File screenshotLocation = new File("./screenshots/" + filePath + "png");
+		FileUtils.copyFile(screenshot, screenshotLocation);
+		Thread.sleep(2000);
+		InputStream is = new FileInputStream(screenshotLocation);
+		byte[] imageBytes = IOUtils.toByteArray(is);
+		Thread.sleep(2000);
+		String base64 = Base64.getEncoder().encodeToString(imageBytes);
+		return base64;
+	}
+
+}
